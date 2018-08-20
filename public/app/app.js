aba6670658caa1ca24db1ef10924a6e7e04d59b0
jefferson.souza <jefferson.souza@synchro.com.br>
2018-08-20 12:53:04 -0300

    Updated code to use composition with pipe function

--- public/app/app.js
+++ public/app/app.js
@@ -1,14 +1,17 @@
 import { log } from './utils/promise-helpers.js';
 import { NotasService } from './invoice/service.js';
-import { takeUntil, debounceTime } from './utils/operators.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
 
-const sumItems = debounceTime(
-  300,
-  takeUntil(3, () =>
-    NotasService.sumItems('2143')
-      .then(log)
-      .catch(console.error)
-  )
+//creates a pipe to execute the functions starting from the last
+const composedOperations = pipe(
+  partialize(takeUntil, 3),
+  partialize(debounceTime, 300)
+);
+
+const sumItems = composedOperations(() =>
+  NotasService.sumItems('2143')
+    .then(log)
+    .catch(console.error)
 );
 
 document.getElementById('btnLoad').onclick = sumItems;
